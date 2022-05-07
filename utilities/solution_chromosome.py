ce774ab1eed48444f355f39854443e861284bf52
William Chen <10979111@gm.nfu.edu.tw>
2022-05-07 12:52:50 +0800

    adjuct sorting logic of SolutionChromosom to conform with thesis objective function

--- utilities/solution_chromosome.py
+++ utilities/solution_chromosome.py
@@ -57,19 +57,26 @@ class SolutionChromosome(BuilderFactory):
     def __repr__(self) -> str:
         chromosome = f"Chromosome: {self.chromosome}"
         resources = self.resources_used
+
         fuel_fee = f"Fuel Fee: ${int(resources['fuel_fee'])}"
         distance = f"Distance: {int(resources['distance'])} km"
         total_delivery_time = f"Total Delivery Time: {round(resources['delivery_time'] + resources['service_time'], 2)} Mins"
         vehicle_fixed_cost = f"Vehicle Fixed Cost: ${int(resources['vehicle_fixed_cost'])}"
         driver_cost = f"Driver Cost: ${int(resources['driver_cost'])}"
+        total_cost_amount = int(
+            resources['fuel_fee']) + resources['vehicle_fixed_cost'] + int(resources['driver_cost'])
+        total_cost = f"Total Cost: ${total_cost_amount}"
         number_of_vehicles_assigned = f"Number of Vehicles Assigned: {resources['number_of_vehicles_assigned']}"
         sep = "-" * 60
+
         _repr = [
             chromosome,
-            fuel_fee, distance,
-            total_delivery_time,
+            total_cost,
+            fuel_fee,
             vehicle_fixed_cost,
             driver_cost,
+            distance,
+            total_delivery_time,
             number_of_vehicles_assigned,
             sep
         ]
@@ -84,23 +91,20 @@ class SolutionChromosome(BuilderFactory):
         # 降低總配送距離與減少每趟次的花費時間為次要目標，
         # 以求取最佳的配送路線。所以目標函數(1)為最小化車輛的固定成本、
         # 車輛行駛的距離成本、每趟次花費的司機員薪資成本。
+        self_resources = self.resources_used
+        other_resources = _other_solution_chromosome.resources_used
 
-        self_distance = self.resources_used["distance"]
-        self_time = (
-            self.resources_used["delivery_time"] +
-            self.resources_used["service_time"]
-        )
-        other_distance = _other_solution_chromosome.resources_used["distance"]
-        other_time = (
-            _other_solution_chromosome.resources_used["delivery_time"] +
-            _other_solution_chromosome.resources_used["service_time"]
-        )
+        self_fuel_fee = self_resources['fuel_fee']
+        self_driver_cost = self_resources['driver_cost']
+        self_vehicle_fixed_cost = self_resources['vehicle_fixed_cost']
+        self_total_cost = self_fuel_fee + self_driver_cost + self_vehicle_fixed_cost
 
-        return (
-            self.number_of_vehicles_assigned < _other_solution_chromosome.number_of_vehicles_assigned
-            or self_distance < other_distance
-            or self_time < other_time
-        )
+        other_fuel_fee = other_resources['fuel_fee']
+        other_driver_cost = other_resources['driver_cost']
+        other_vehicle_fixed_cost = other_resources['vehicle_fixed_cost']
+        other_total_cost = other_fuel_fee + other_driver_cost + other_vehicle_fixed_cost
+
+        return self_total_cost < other_total_cost
 
     @ property
     def fitness(self):
